a8387efcb3ad8090bb9039fb5b2614f538800539
ulascankutay <ulascankutay@gmail.com>
2024-05-30 13:28:58 +0300

    Control

--- main.py
+++ main.py
@@ -23,10 +23,73 @@ def subscribe_callback(topic,message):
     
     message_str = message.decode('utf-8')
     
-    if topic==b"home/led" and message_str=="1":
-        p0.on()
-    elif topic==b"home/led" and message_str=="0":
-        p0.off()
+    if topic == b"home/led":
+        if message_str == "1":
+            # LED açık
+            print("LED açık")
+        elif message_str == "0":
+            # LED kapalı
+            print("LED kapalı")
+    elif topic == b"home/temperature":
+        if message_str == "1":
+            # Geçerli sıcaklık verisi
+            print("Geçerli sıcaklık verisi")
+        elif message_str == "0":
+            # Geçersiz sıcaklık verisi
+            print("Geçersiz sıcaklık verisi")
+    elif topic == b"home/humidity":
+        if message_str == "1":
+            # Geçerli nem verisi
+            print("Geçerli nem verisi")
+        elif message_str == "0":
+            # Geçersiz nem verisi
+            print("Geçersiz nem verisi")
+    elif topic == b"home/fan":
+        if message_str == "1":
+            # Fan açık
+            print("Fan açık")
+        elif message_str == "0":
+            # Fan kapalı
+            print("Fan kapalı")
+    elif topic == b"home/door":
+        if message_str == "open":
+            # Kapı açık
+            print("Kapı açık")
+        elif message_str == "close":
+            # Kapı kapalı
+            print("Kapı kapalı")
+    elif topic == b"bahce/nem":
+        if message_str == "1":
+            # Geçerli bahçe nem verisi
+            print("Geçerli bahçe nem verisi")
+        elif message_str == "0":
+            # Geçersiz bahçe nem verisi
+            print("Geçersiz bahçe nem verisi")
+    elif topic == b"bahce/supom":
+        if message_str == "1":
+            # Bahçe su pompası açık
+            print("Bahçe su pompası açık")
+        elif message_str == "0":
+            # Bahçe su pompası kapalı
+            print("Bahçe su pompası kapalı")
+    elif topic == b"home/kapı":
+        if message_str == "open":
+            # Ev kapısı açık
+            print("Ev kapısı açık")
+        elif message_str == "close":
+            # Ev kapısı kapalı
+            print("Ev kapısı kapalı")
+    elif topic == b"home/pencere":
+        if message_str == "open":
+            # Ev penceresi açık
+            print("Ev penceresi açık")
+        elif message_str == "close":
+            # Ev penceresi kapalı
+            print("Ev penceresi kapalı")
+    else:
+        # Bilinmeyen mesaj veya topik
+        print("Bilinmeyen mesaj veya topik")
+
 
 def ConnectAndSubscribe():
     sslparams = {'server_hostname': secrets["broker"]}
