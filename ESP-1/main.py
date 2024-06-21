35c9d8812f2e3828fe153ee8fb8837a59b38dc2a
ulascankutay <ulascankutay@gmail.com>
2024-06-21 23:51:33 +0300

    FINAL

--- ESP-1/main.py
+++ ESP-1/main.py
@@ -1,24 +1,32 @@
 from simple import MQTTClient
 import urequests as requests
-from machine import Pin
 from time import sleep
-import network
+from machine import ADC,Pin
+from servo import Servo
 import machine
 import sensor
 
+buzzer = Pin(32, Pin.OUT)
+LED1 = Pin(2, Pin.OUT)
+LED2 = Pin(18, Pin.OUT)
+relay1 = Pin(12, Pin.OUT)
+relay2 = Pin(14, Pin.OUT)
+relay3 = Pin(27, Pin.OUT)
+relay4 = Pin(26, Pin.OUT)
+motor=Servo(pin=25) 
 
+relay1.value(1)
+relay2.value(1)
+relay3.value(1)
+relay4.value(1)
+        
 
-
-
-LED1 = Pin(3, Pin.OUT)
-         
 secrets = {
    'mqtt_username' : 'ulascan',
    'mqtt_key' : '135790.Uk',
    'broker' : '59861b68801a421591bfed24cac1febf.s1.eu.hivemq.cloud',
    'port' : 8883
 }
-
 
 def subscribe_callback(topic,message):
     
@@ -27,73 +35,44 @@ def subscribe_callback(topic,message):
     message_str = message.decode('utf-8')
     
     if topic == b"home/led":
+        if message_str == "11":
+            LED1.on()        
+        elif message_str == "10":
+            LED1.off()    
+        elif message_str == "21":
+            LED2.on()
+        elif message_str == "20":
+            LED2.off()
+    elif topic == b"home/role":
+        if message_str == "10":
+            relay1.value(1)        
+        elif message_str == "11":
+            relay1.value(0)   
+        elif message_str == "20":
+            relay2.value(1)
+        elif message_str == "21":
+            relay2.value(0)
+        elif message_str == "30":
+            relay3.value(1)
+        elif message_str == "31":
+            relay3.value(0)
+        elif message_str == "40":
+            relay4.value(1)
+        elif message_str == "41":
+            relay4.value(0)
+    elif topic == b"home/alarm":
         if message_str == "1":
-            LED1.on()
-            print("LED açık")
+            buzzer.on()
         elif message_str == "0":
-            LED1.off()
-            print("LED kapalı")
-    elif topic == b"home/temperature":
-        if message_str == "1":
-            # Geçerli sıcaklık verisi
-            print("Geçerli sıcaklık verisi")
-        elif message_str == "0":
-            # Geçersiz sıcaklık verisi
-            print("Geçersiz sıcaklık verisi") 
-    elif topic == b"home/humidity":
-        if message_str == "1":
-            # Geçerli nem verisi
-            print("Geçerli nem verisi")
-        elif message_str == "0":
-            # Geçersiz nem verisi
-            print("Geçersiz nem verisi")
-    elif topic == b"home/fan":
-        if message_str == "1":
-            # Fan açık
-            print("Fan açık")
-        elif message_str == "0":
-            # Fan kapalı
-            print("Fan kapalı")
-    elif topic == b"home/door":
-        if message_str == "open":
-            # Kapı açık
-            print("Kapı açık")
-        elif message_str == "close":
-            # Kapı kapalı
-            print("Kapı kapalı")
-    elif topic == b"bahce/nem":
-        if message_str == "1":
-            # Geçerli bahçe nem verisi
-            print("Geçerli bahçe nem verisi")
-        elif message_str == "0":
-            # Geçersiz bahçe nem verisi
-            print("Geçersiz bahçe nem verisi")
-    elif topic == b"bahce/supom":
-        if message_str == "1":
-            # Bahçe su pompası açık
-            print("Bahçe su pompası açık")
-        elif message_str == "0":
-            # Bahçe su pompası kapalı
-            print("Bahçe su pompası kapalı")
+            buzzer.off()
     elif topic == b"home/kapi":
-        if message_str == "open":
-            # Ev kapısı açık
-            print("Ev kapısı açık")
-        elif message_str == "close":
-            # Ev kapısı kapalı
-            print("Ev kapısı kapalı")
-    elif topic == b"home/pencere":
-        if message_str == "open":
-            # Ev penceresi açık
-            print("Ev penceresi açık")
-        elif message_str == "close":
-            # Ev penceresi kapalı
-            print("Ev penceresi kapalı")
+        if message_str == "1":
+           motor.move(180)
+        elif message_str == "0":
+           motor.move(0)
     else:
-        # Bilinmeyen mesaj veya topik
         print("Bilinmeyen mesaj veya topik")
-
-
+        
 def ConnectAndSubscribe():
     sslparams = {'server_hostname': secrets["broker"]}
     client = MQTTClient(client_id="esp32",
@@ -106,50 +85,47 @@ def ConnectAndSubscribe():
                     ssl_params=sslparams) 
     client.set_callback(subscribe_callback)
     client.connect()
-    client.subscribe(b"home/siren")
-    client.subscribe(b"home/ldr")
+    client.subscribe(b"home/led")
+    client.subscribe(b"home/role")
+    client.subscribe(b"home/alarm")
     client.subscribe(b"home/sicaklik")
     client.subscribe(b"home/mq2")
-    client.subscribe(b"home/role")
-    client.subscribe(b"home/ldr")
     client.subscribe(b"home/fan")
     client.subscribe(b"home/kapi")
-    client.subscribe(b"home/pencere")
-    client.subscribe(b"home/hareket")
-    client.subscribe(b"bahce/yagmur")
-    client.subscribe(b"bahce/supom")
     return client
-
 
 def publish(topic,value):
     print("-----------")
     client.publish(topic,str(value))
     print(" {} topigine {}'degeri gonderildi!".format(topic,value))
     print("-----------")
-    
-    
+     
 def RestartAndConnect():
     sleep(2)
     machine.reset()
-
 
 
 try:
     client = ConnectAndSubscribe()
 except OSError as e:
     RestartAndConnect()
-cnt = 0
+   
+publish("ESP-1", 1)
 while True :
-        #sensor.temperature()
-        #sensor.humidity()
-        
-        client.wait_msg()
+    temp,hum = sensor.dht_sensor()
+    sleep(0.5)
+    if temp >= 35 or hum >= 40:
+        publish("home/fan",11)
         sleep(0.2)
-       
-        dht_readings = {'field1':temp,'field2':hum,'field3':cnt}
-        request = requests.post('https://api.thingspeak.com/update?api_key=ZT0I1PTFIIIBB0GE',json=dht_readings, headers = {'Content-Type': 'application/json'})
-        request.close()
-        sleep(0.2)
+        publish("home/fan",21)
+    cnt = sensor.mq_sensor()
+    client.check_msg()
+    sleep(0.5)
+    
+    dht_readings = {'field1':temp,'field2':hum,'field3':cnt}
+    request = requests.post('https://api.thingspeak.com/update?api_key=ZT0I1PTFIIIBB0GE',json=dht_readings, headers = {'Content-Type': 'application/json'})
+    request.close()
+    sleep(0.2)
 
     
     
